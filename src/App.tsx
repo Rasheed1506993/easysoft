c4e3ed1178ef7e9a0b7b9fc81973686f940d637f
Rasheed1506993 <rashed734002725@gmail.com>
2025-06-10 16:40:43 +0000

    h

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="easysoft">
         <Layout>
           <Routes>
             <Route path="/" element={<Index />} />
